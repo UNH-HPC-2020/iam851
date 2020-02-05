24e4d47b8bfda67795ccc7844763ce0b57d92456
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-02-05 13:59:54 -0500

    class5: make another copy of the hello project, this time with cmake

---
+++ class5/factorial.cxx
@@ -0,0 +1,15 @@
+
+#include "hello.h"
+
+int
+factorial(int n)
+{
+  int result;
+  
+  if (n = 0) {
+    result = 1;
+  } else {
+    result = n * factorial(n-1);
+  }
+}
+
---
+++ class5/greeting.cxx
@@ -0,0 +1,11 @@
+
+#include "hello.h"
+
+#include <stdio.h>
+
+void
+greeting(const char *name)
+{
+  printf("Hi there, %s.\n", name);
+}
+
