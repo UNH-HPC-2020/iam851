7fd9102cc4e52ee9fabde0ee137a50e859283ec8
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-02-24 16:38:27 -0500

    class10: add vector_print function
    It's not really being used right now (all we do is tests, and they're automatic), but it might be handy in the future.

--- linear_algebra/c/vector.c
+++ linear_algebra/c/vector.c
@@ -1,6 +1,7 @@
 
 #include "linear_algebra.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 
 void vector_construct(struct vector *v, int n)
@@ -12,4 +13,13 @@ void vector_construct(struct vector *v, int n)
 void vector_destruct(struct vector *v)
 {
   free(v->data);
+}
+
+void vector_print(const struct vector *v)
+{
+  printf("{");
+  for (int i = 0; i < v->n; i++) {
+    printf(" %g", VEC(v, i));
+  }
+  printf(" }\n");
 }
