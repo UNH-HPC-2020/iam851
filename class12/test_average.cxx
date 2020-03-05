93b71067fca5f0a86de3aea331020759c9d69370
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-03-04 21:57:54 -0500

    class14: test_average now working

--- class12/test_average.cxx
+++ class12/test_average.cxx
@@ -1,24 +1,39 @@
 
+#include <xtensor/xio.hpp>
 #include <xtensor/xtensor.hpp>
 
 #include <cmath>
 #include <fstream>
 #include <iostream>
 
-const size_t N = 50;
+// ugh, global variables are ugly
+
+const size_t N = 30;
 const double dx = 2. * M_PI / N;
 
-xt::xtensor<double, 1> make_func_nc()
+using vector = xt::xtensor<double, 1>;
+
+vector make_func_nc()
 {
-  xt::xtensor<double, 1>::shape_type shape = {N + 1};
-  auto f = xt::empty<double>(shape);
+  vector f_nc = xt::empty<double>({N + 1});
 
   for (int i = 0; i <= N; i++) {
     double x = i * dx;
-    f(i) = sin(x) + (1./3.) * sin(3 * x);
+    f_nc(i) = sin(x) + (1. / 3.) * sin(3 * x);
   }
 
-  return f;
+  return f_nc;
+}
+
+vector avg(const vector& f_nc)
+{
+  vector f_cc = xt::empty<double>({N});
+
+  for (int i = 0; i < N; i++) {
+    f_cc(i) = .5 * (f_nc(i) + f_nc(i + 1));
+  }
+
+  return f_cc;
 }
 
 void write_nc(const xt::xtensor<double, 1>& f, const std::string& filename)
@@ -29,10 +44,19 @@ void write_nc(const xt::xtensor<double, 1>& f, const std::string& filename)
   }
 }
 
+void write_cc(const xt::xtensor<double, 1>& f, const std::string& filename)
+{
+  std::ofstream of(filename);
+  for (int i = 0; i < f.shape(0); i++) {
+    of << (i + .5) * dx << "\t" << f(i) << "\n";
+  }
+}
+
 int main(int argc, char** argv)
 {
-  const int N = 20;
-
   auto f_nc = make_func_nc();
   write_nc(f_nc, "f_nc.asc");
+
+  auto f_cc = avg(f_nc);
+  write_cc(f_cc, "f_cc.asc");
 }
