ed70a1cb9094170af7cde066f6c550f2ecc10a32
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-03-04 22:49:16 -0500

    class13hw: change data layout to column-major

--- linear_algebra/cxx/linear_algebra.h
+++ linear_algebra/cxx/linear_algebra.h
@@ -5,7 +5,7 @@
 #include <xtensor/xtensor.hpp>
 
 using vector = xt::xtensor<double, 1>;
-using matrix = xt::xtensor<double, 2>;
+using matrix = xt::xtensor<double, 2, xt::layout_type::column_major>;
 
 double dot(const vector& x, const vector& y);
 vector dot(const matrix& A, const vector& x);
