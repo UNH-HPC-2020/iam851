b3c872ba6eac2fb6be2e28bc726e9063d3ed8fe8
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-03-04 21:16:24 -0500

    class14: start test_average
    It's just setting the original function, and writing it

---
+++ class12/test_average.cxx
@@ -0,0 +1,38 @@
+
+#include <xtensor/xtensor.hpp>
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+const size_t N = 50;
+const double dx = 2. * M_PI / N;
+
+xt::xtensor<double, 1> make_func_nc()
+{
+  xt::xtensor<double, 1>::shape_type shape = {N + 1};
+  auto f = xt::empty<double>(shape);
+
+  for (int i = 0; i <= N; i++) {
+    double x = i * dx;
+    f(i) = sin(x) + (1./3.) * sin(3 * x);
+  }
+
+  return f;
+}
+
+void write_nc(const xt::xtensor<double, 1>& f, const std::string& filename)
+{
+  std::ofstream of(filename);
+  for (int i = 0; i < f.shape(0); i++) {
+    of << i * dx << "\t" << f(i) << "\n";
+  }
+}
+
+int main(int argc, char** argv)
+{
+  const int N = 20;
+
+  auto f_nc = make_func_nc();
+  write_nc(f_nc, "f_nc.asc");
+}
