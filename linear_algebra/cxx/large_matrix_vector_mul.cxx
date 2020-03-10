438873e093b370b6f85b4fcba41dcf43a43598c1
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-03-09 20:43:22 -0400

    class13hw: add timing

--- linear_algebra/cxx/large_matrix_vector_mul.cxx
+++ linear_algebra/cxx/large_matrix_vector_mul.cxx
@@ -4,8 +4,14 @@
 int main(int argc, char** argv)
 {
   const int N = 20000;
+  double t1b = Wtime();
   vector x = xt::arange(N);
   matrix A = xt::eye(N);
+  double t1e = Wtime();
+  std::cout << "init took " << t1e - t1b << "\n";
 
+  double t2b = Wtime();
   vector y = dot(A, x);
+  double t2e = Wtime();
+  std::cout << "dot took " << t2e - t2b << "\n";
 }
