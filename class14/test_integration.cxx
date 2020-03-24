e2385e2a43f70bacea0cf079f52cad8edda3c1b4
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-03-24 10:18:45 -0400

    class16hw: use omp critical
    correct, but real slow...

--- class14/test_integration.cxx
+++ class14/test_integration.cxx
@@ -19,12 +19,11 @@ int main(int argc, char** argv)
   double t_beg = Wtime();
 #pragma omp parallel
   {
-    double local_sum = 0.;
 #pragma omp for
     for (int i = 0; i < N; i++) {
-      local_sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
+#pragma omp critical
+      sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
     }
-    sum += local_sum;
   }
   double t_end = Wtime();
   std::cout << "took " << t_end - t_beg << " sec.\n";
