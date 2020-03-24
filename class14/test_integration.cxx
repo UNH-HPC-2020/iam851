bd4a3258ba25521ef764d824321dff53850e5755
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-03-24 10:01:26 -0400

    class15hw: parallelize test_integration

--- class14/test_integration.cxx
+++ class14/test_integration.cxx
@@ -1,4 +1,6 @@
 
+#include "wtime.h"
+
 #include <cmath>
 #include <iostream>
 
@@ -9,13 +11,18 @@ double f(double x)
 
 int main(int argc, char** argv)
 {
-  const int N = 10;
+  const int N = 100000000;
 
   double sum = 0.;
   double dx = 1. / N;
+
+  double t_beg = Wtime();
+#pragma omp parallel for
   for (int i = 0; i < N; i++) {
     sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
   }
+  double t_end = Wtime();
+  std::cout << "took " << t_end - t_beg << " sec.\n";
 
   std::cout << "integral is approximately " << sum << "\n";
 }
