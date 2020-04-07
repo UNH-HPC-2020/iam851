bf85701bde911ceaeaf568b83c76ae8a97df8e09
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-07 10:15:39 -0400

    class18hw: get rank, size and print them
    This actually crashes, though :(

--- class18/mpi_hello.cxx
+++ class18/mpi_hello.cxx
@@ -1,9 +1,15 @@
 
 #include <iostream>
+#include <mpi.h>
 
 int main(int argc, char** argv)
 {
-  std::cout << "Hi there!\n";
+  int rank, size;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  std::cout << "Hi there " << rank << "/" << size << "\n";
 
   return 0;
 }
