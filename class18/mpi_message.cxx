3f24d9b1ad87d3bc9a0cbe29d9abd20110184144
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-07 10:20:58 -0400

    class18hw: actually send and receive one message

--- class18/mpi_message.cxx
+++ class18/mpi_message.cxx
@@ -15,6 +15,9 @@ int main(int argc, char** argv)
 
   if (rank == 0) {
     test = 99;
+    MPI_Send(&test, 1, MPI_INT, 3, 123, MPI_COMM_WORLD);
+  } else if (rank == 3) {
+    MPI_Recv(&test, 1, MPI_INT, 0, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   }
 
   std::cout << "Hi there " << rank << "/" << size << " test = " << test << "\n";
