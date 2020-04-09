25dce81e0a7ab12934ff7e7a4a79323b57fa8595
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-08 22:24:53 -0400

    class19hw: use only 100 trapezoids, remove OpenMP
    A smaller case never hurts during development/debugging, and we don't
    want to use OpenMP anymore.

--- class18/test_integration.cxx
+++ class18/test_integration.cxx
@@ -10,13 +10,12 @@ double f(double x)
 
 int main(int argc, char** argv)
 {
-  const int N = 100000000;
+  const int N = 100;
 
   double sum = 0.;
   double dx = 1. / N;
 
   double t_beg = MPI_Wtime();
-#pragma omp parallel for reduction(+ : sum)
   for (int i = 0; i < N; i++) {
     sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
   }
