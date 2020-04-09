0099dd995b09bee0fe27dd0306b73f7fe0dda6fa
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-08 22:29:33 -0400

    class19hw: actually split the work
    This needs to be run on exactly two procs, it does then calculate
    partial results on each MPI proc -- but doesn't put them back together yet.

--- class18/test_integration.cxx
+++ class18/test_integration.cxx
@@ -1,4 +1,5 @@
 
+#include <cassert>
 #include <cmath>
 #include <iostream>
 #include <mpi.h>
@@ -18,12 +19,20 @@ int main(int argc, char** argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+  assert(size == 2); // needs to be run on exactly two procs for now
+
   double sum = 0.;
   double dx = 1. / N;
 
   double t_beg = MPI_Wtime();
-  for (int i = 0; i < N; i++) {
-    sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
+  if (rank == 0) {
+    for (int i = 0; i < 50; i++) {
+      sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
+    }
+  } else if (rank == 1) {
+    for (int i = 50; i < 100; i++) {
+      sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
+    }
   }
   double t_end = MPI_Wtime();
   std::cout << "took " << t_end - t_beg << " sec.\n";
