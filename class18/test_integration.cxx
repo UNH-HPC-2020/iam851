2934ac737e3412fc52dcba5b697aa7563f392665
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-08 22:23:09 -0400

    class19hw: compile with MPI, use MPI_Wtime
    It doesn't really use MPI yet, except for the timing. But it works.

--- class18/test_integration.cxx
+++ class18/test_integration.cxx
@@ -1,8 +1,7 @@
 
-#include "wtime.h"
-
 #include <cmath>
 #include <iostream>
+#include <mpi.h>
 
 double f(double x)
 {
@@ -16,12 +15,12 @@ int main(int argc, char** argv)
   double sum = 0.;
   double dx = 1. / N;
 
-  double t_beg = Wtime();
-#pragma omp parallel for reduction(+:sum)
+  double t_beg = MPI_Wtime();
+#pragma omp parallel for reduction(+ : sum)
   for (int i = 0; i < N; i++) {
     sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
   }
-  double t_end = Wtime();
+  double t_end = MPI_Wtime();
   std::cout << "took " << t_end - t_beg << " sec.\n";
 
   std::cout << "integral is approximately " << sum << "\n";
