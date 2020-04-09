cf21bf806fbe22e9fee75c7cb8810002ae0afe3b
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-08 22:20:57 -0400

    class19hw: copy test_integration.cxx from class14/
    It doesn't actually compile because wtime.h is missing.

---
+++ class18/test_integration.cxx
@@ -0,0 +1,28 @@
+
+#include "wtime.h"
+
+#include <cmath>
+#include <iostream>
+
+double f(double x)
+{
+  return std::sqrt(1. - x * x);
+}
+
+int main(int argc, char** argv)
+{
+  const int N = 100000000;
+
+  double sum = 0.;
+  double dx = 1. / N;
+
+  double t_beg = Wtime();
+#pragma omp parallel for reduction(+:sum)
+  for (int i = 0; i < N; i++) {
+    sum += .5 * dx * (f(i * dx) + f((i + 1) * dx));
+  }
+  double t_end = Wtime();
+  std::cout << "took " << t_end - t_beg << " sec.\n";
+
+  std::cout << "integral is approximately " << sum << "\n";
+}
