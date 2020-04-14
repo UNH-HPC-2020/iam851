8c6b87c9b297fc8ba77cd83d6fc0e244f2cfe193
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-14 11:23:13 -0400

    class21hw: start MPIDomain class
    it's only a wrapper for the communicator and rank, size for now, so
    it doesn't really make things much simpler...

--- class20/test_derivative.cxx
+++ class20/test_derivative.cxx
@@ -6,29 +6,47 @@
 
 #include <mpi.h>
 
+#include <cassert>
 #include <fstream>
 #include <iostream>
-#include <cassert>
 
-xt::xtensor<double, 1> derivative(const xt::xtensor<double, 1>& f, double dx)
+class MPIDomain
+{
+public:
+  MPIDomain(MPI_Comm comm) : comm_(comm)
+  {
+    MPI_Comm_rank(comm_, &rank_);
+    MPI_Comm_size(comm_, &size_);
+  }
+
+  MPI_Comm comm() const { return comm_; }
+  int rank() const { return rank_; }
+  int size() const { return size_; }
+
+private:
+  MPI_Comm comm_;
+  int rank_;
+  int size_;
+};
+
+xt::xtensor<double, 1> derivative(const MPIDomain& domain,
+                                  const xt::xtensor<double, 1>& f, double dx)
 {
   const int G = 1;
-  
+
   int n = f.shape(0);
   auto f_g = xt::pad(f, G);
 
   // fill ghosts
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  int left = domain.rank() > 0 ? domain.rank() - 1 : domain.size() - 1;
+  int right = domain.rank() < domain.size() - 1 ? domain.rank() + 1 : 0;
+  MPI_Send(&f_g(G + 0), 1, MPI_DOUBLE, left, 123, domain.comm());
+  MPI_Recv(&f_g(G + n), 1, MPI_DOUBLE, right, 123, domain.comm(),
+           MPI_STATUS_IGNORE);
 
-  int left = rank > 0 ? rank - 1 : size - 1;
-  int right = rank < size - 1 ? rank + 1 : 0;
-  MPI_Send(&f_g(G + 0), 1, MPI_DOUBLE, left, 123, MPI_COMM_WORLD);
-  MPI_Recv(&f_g(G + n), 1, MPI_DOUBLE, right, 123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-  MPI_Send(&f_g(G + n - 1), 1, MPI_DOUBLE, right, 456, MPI_COMM_WORLD);
-  MPI_Recv(&f_g(G + -1), 1, MPI_DOUBLE, left, 456, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  MPI_Send(&f_g(G + n - 1), 1, MPI_DOUBLE, right, 456, domain.comm());
+  MPI_Recv(&f_g(G + -1), 1, MPI_DOUBLE, left, 456, domain.comm(),
+           MPI_STATUS_IGNORE);
 
 #if 1
   auto fprime = xt::zeros_like(f);
@@ -49,25 +67,23 @@ int main(int argc, char** argv)
   const int N = 16; // number of grid points
 
   MPI_Init(&argc, &argv);
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPIDomain domain(MPI_COMM_WORLD);
 
-  assert(N % size == 0); // # grid points must be divisible by # procs
-  int n = N / size;
-  
+  assert(N % domain.size() == 0); // # grid points must be divisible by # procs
+  int n = N / domain.size();
+
   // create coordinates [0, 2pi)
   double L = 2. * M_PI; // total size of domain
   double dx = L / N;
-  auto x = xt::arange<double>(rank * n, (rank + 1) * n) * dx;
+  auto x = xt::arange<double>(domain.rank() * n, (domain.rank() + 1) * n) * dx;
 
   // our original function f
   auto f = sin(x);
 
   // calculate derivative
-  auto fprime = derivative(f, dx);
+  auto fprime = derivative(domain, f, dx);
 
-  std::ofstream out("f-" + std::to_string(rank) + ".csv");
+  std::ofstream out("f-" + std::to_string(domain.rank()) + ".csv");
   xt::dump_csv(out, xt::stack(xt::xtuple(x, f, fprime), 1));
 
   MPI_Finalize();
