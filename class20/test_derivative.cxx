98429521d19447d248ca467fcf6b544195249332
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-14 11:29:43 -0400

    class21hw: handle number of grid points in MPIDomain

--- class20/test_derivative.cxx
+++ class20/test_derivative.cxx
@@ -13,20 +13,28 @@
 class MPIDomain
 {
 public:
-  MPIDomain(MPI_Comm comm) : comm_(comm)
+  MPIDomain(MPI_Comm comm, int N) : comm_(comm), N_(N)
   {
     MPI_Comm_rank(comm_, &rank_);
     MPI_Comm_size(comm_, &size_);
+
+    assert(N % size_ == 0); // # grid points must be divisible by # procs
+    n_ = N / size_;
   }
 
   MPI_Comm comm() const { return comm_; }
   int rank() const { return rank_; }
   int size() const { return size_; }
 
+  int N() const { return N_; }
+  int n() const { return n_; }
+
 private:
   MPI_Comm comm_;
   int rank_;
   int size_;
+  int N_; // global number of grid points
+  int n_; // local number of grid points
 };
 
 xt::xtensor<double, 1> derivative(const MPIDomain& domain,
@@ -34,8 +42,10 @@ xt::xtensor<double, 1> derivative(const MPIDomain& domain,
 {
   const int G = 1;
 
-  int n = f.shape(0);
   auto f_g = xt::pad(f, G);
+
+  int n = domain.n();
+  assert(f.shape(0) == n);
 
   // fill ghosts
   int left = domain.rank() > 0 ? domain.rank() - 1 : domain.size() - 1;
@@ -67,15 +77,14 @@ int main(int argc, char** argv)
   const int N = 16; // number of grid points
 
   MPI_Init(&argc, &argv);
-  MPIDomain domain(MPI_COMM_WORLD);
-
-  assert(N % domain.size() == 0); // # grid points must be divisible by # procs
-  int n = N / domain.size();
+  MPIDomain domain(MPI_COMM_WORLD, N);
 
   // create coordinates [0, 2pi)
   double L = 2. * M_PI; // total size of domain
   double dx = L / N;
-  auto x = xt::arange<double>(domain.rank() * n, (domain.rank() + 1) * n) * dx;
+  auto x = xt::arange<double>(domain.rank() * domain.n(),
+                              (domain.rank() + 1) * domain.n()) *
+           dx;
 
   // our original function f
   auto f = sin(x);
