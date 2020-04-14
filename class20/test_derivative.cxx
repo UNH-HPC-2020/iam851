82f146be43793e482d886bd5083bc2e7ddf61d94
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-04-14 11:33:45 -0400

    class21hw: MPIDomain handle domain length, grid spacing

--- class20/test_derivative.cxx
+++ class20/test_derivative.cxx
@@ -13,13 +13,14 @@
 class MPIDomain
 {
 public:
-  MPIDomain(MPI_Comm comm, int N) : comm_(comm), N_(N)
+  MPIDomain(MPI_Comm comm, int N, double L) : comm_(comm), N_(N), L_(L)
   {
     MPI_Comm_rank(comm_, &rank_);
     MPI_Comm_size(comm_, &size_);
 
     assert(N % size_ == 0); // # grid points must be divisible by # procs
     n_ = N / size_;
+    dx_ = L / N_;
   }
 
   MPI_Comm comm() const { return comm_; }
@@ -28,23 +29,27 @@ public:
 
   int N() const { return N_; }
   int n() const { return n_; }
+  double dx() const { return dx_; }
 
 private:
   MPI_Comm comm_;
   int rank_;
   int size_;
-  int N_; // global number of grid points
-  int n_; // local number of grid points
+  int N_;     // global number of grid points
+  int n_;     // local number of grid points
+  double L_;  // global domain length
+  double dx_; // grid spacing
 };
 
 xt::xtensor<double, 1> derivative(const MPIDomain& domain,
-                                  const xt::xtensor<double, 1>& f, double dx)
+                                  const xt::xtensor<double, 1>& f)
 {
   const int G = 1;
 
   auto f_g = xt::pad(f, G);
 
   int n = domain.n();
+  double dx = domain.dx();
   assert(f.shape(0) == n);
 
   // fill ghosts
@@ -77,20 +82,18 @@ int main(int argc, char** argv)
   const int N = 16; // number of grid points
 
   MPI_Init(&argc, &argv);
-  MPIDomain domain(MPI_COMM_WORLD, N);
+  MPIDomain domain(MPI_COMM_WORLD, N, 2. * M_PI);
 
-  // create coordinates [0, 2pi)
-  double L = 2. * M_PI; // total size of domain
-  double dx = L / N;
+  // create coordinates
   auto x = xt::arange<double>(domain.rank() * domain.n(),
                               (domain.rank() + 1) * domain.n()) *
-           dx;
+           domain.dx();
 
   // our original function f
   auto f = sin(x);
 
   // calculate derivative
-  auto fprime = derivative(domain, f, dx);
+  auto fprime = derivative(domain, f);
 
   std::ofstream out("f-" + std::to_string(domain.rank()) + ".csv");
   xt::dump_csv(out, xt::stack(xt::xtuple(x, f, fprime), 1));
